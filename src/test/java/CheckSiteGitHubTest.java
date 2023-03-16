f5e70f5dbda6dfa1eacec382ce110c4430cec0ea
Vladislav Kolesnikov <vladislavpaokin@gmail.com>
2023-03-16 19:17:28 +0300

    added annotation in CsvSource

--- src/test/java/CheckSiteGitHubTest.java
+++ src/test/java/CheckSiteGitHubTest.java
@@ -29,17 +29,15 @@ public class CheckSiteGitHubTest {
         //issues-tab
     }
     @CsvSource({
-            "Selenide,         #issues-tab",
-            "Allure-Java,       #issues-tab",
+            "Selenide,         #issues-tab,         Issues",
+            "Allure-Java,       #issues-tab,       Issues",
     })
-    @ParameterizedTest(name="Проверка страницы репозитория {0} на наличие страницы Code")
-    public void checkHeaderRepoTestTabIssue(String nameRepo, String tab){
+    @ParameterizedTest(name="Проверка страницы репозитория {0} на наличие страницы {1} с наименованием {2}")
+    public void checkHeaderRepoTestTabIssue(String nameRepo, String tab, String nameButton){
         $("[placeholder='Search GitHub']").click();
         $("[placeholder='Search GitHub']").setValue(nameRepo).pressEnter();
         $$("ul.repo-list li").first().$("a").click();
-        $(tab).shouldBe(Condition.visible);
-        sleep(4000);
-        //issues-tab
+        $(tab).shouldHave(Condition.text(nameButton));
     }
 }
 
